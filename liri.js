e29b9ddb520ce2b2d1e031fc85db11709c10af87
Cate Ashley <catemarie.ashley@gmail.com>
2019-07-18 23:51:52 -0600

    finished bands in town, spotify, and omdb

--- liri.js
+++ liri.js
@@ -1,72 +1,105 @@
-// require("dotenv").config();
 
-// var keys = require("./keys.js");
+require("dotenv").config();
+
+// Required Packages
+var axios = require("axios");
+var keys = require("./keys");
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
+// Global Variables
+var artistConcert = process.argv[3];
+var concertURL =
+    "https://rest.bandsintown.com/artists/" +
+    artistConcert +
+    "/events?app_id=codingbootcamp";
+var movieName = process.argv[3];
+var movieURL =
+    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+var searchTerm = process.argv[2];
+
+
+
+
 
 // Spotify Section
-// var spotify = new Spotify(keys.spotify);
-
-// search: function ({ 
-//     type: 'artist OR album OR track', 
-//     query: 'My search query', 
-//     limit: 20 
-// }, callback);
-
-
-// spotify.search ({ 
-//     type: 'Song name', 
-//     query: 'My search query',
-//     limit: 20
-//         }, function (err, data) {
-//     if (err) {
-//         return console.log('Error occurred: ' + err);
-//     }
-//     console.log(data);
-// });
-
-
-
-
-
-
-
-
-
+function spotifySearch() {
+    spotify
+    .search ({ 
+        type: 'track', 
+        query: process.argv[3],
+        limit: 1
+        }, function (err, data) {
+            if (!err) {
+                var songObj = data.tracks.items[0];
+                var artistName = songObj.album.artists[0].name;
+                console.log(`
+                Artist: ${artistName}
+                Song title: ${songObj.name}
+                Album Name: ${songObj.album.name}
+                Preview URL: ${songObj.preview_url}`);
+            } 
+            else {
+                console.log(`Error: ${err}`);
+            }
+        })
+};
+    
 
 // OMDB and Axios Section
-var axios = require("axios");
 
-// OMDB movie-name 
-var movieName = process.argv[3];
+function omdbInfo() {
+    axios.get(movieURL).then(
+        function(response) {
+            console.log("Movie Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+            console.log("Production Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Cast: " + response.data.Actors);
+        })
+        .catch(function(error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        })
+    };
 
-// OMDB URL
-var movieURL =
-  "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// Bands In Town and Axios
 
-//   Debug URL
-console.log(movieURL);
-
-axios.get(movieURL).then(
-    function(response) {
-        console.log("Movie Title: " + response.data.Title);
-        console.log("Release Year: " + response.data.Year);
-        console.log("IMDB Rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-        console.log("Production Country: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Cast: " + response.data.Actors);
-    })
+function concertBands() {
+    axios.get(concertURL).then(
+        function(response) {
+            console.log(`
+            Venue Name: ${response.data[0].description}
+            Venue Location: ${response.data[0].venue.city +
+            ", " +
+            response.data[0].venue.region}
+            Date: ${moment(response.data[0].datetime).format("MM/DD/YYYY")}
+            `);
+        }
+    )
     .catch(function(error) {
         if (error.response) {
             console.log(error.response.data);
             console.log(error.response.status);
             console.log(error.response.headers);
-        } else if (error.resquest) {
+        } else if (error.request) {
             console.log(error.request);
         } else {
             console.log("Error", error.message);
         }
         console.log(error.config);
-    });
-
+    })
+};
 
